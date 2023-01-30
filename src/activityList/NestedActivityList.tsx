f79b2c5b41332c037a0f59cea6fdc9f46eb4c690
Thijs Wiefferink <thijs@wiefferink.me>
2023-01-30 22:19:58 +0100

    Store activityType filter in the url

--- src/activityList/NestedActivityList.tsx
+++ src/activityList/NestedActivityList.tsx
@@ -5,17 +5,47 @@ import allActivities from '../activities/allActivities';
 import ActivityList from './ActivityList';
 
 function NestedActivityList() {
-	const [activityTypeFilter, setActivityTypeFilter] = React.useState<null | ActivityType>(null);
+	const [activityTypeFilter, setActivityTypeFilter] = React.useState<null | ActivityType>(getActivityTypeFromUrl());
+
+	const setActivityType = React.useCallback(
+		(newActivityType: null | ActivityType) => {
+			setActivityTypeFilter(newActivityType);
+
+			// Update the url parameter in-place, without reloading the page
+			const url = new URL(window.location.href);
+			if (newActivityType) {
+				url.searchParams.set('activityType', newActivityType);
+			} else {
+				url.searchParams.delete('activityType');
+			}
+			window.history.replaceState({}, 'Home', url);
+		},
+		[setActivityTypeFilter]
+	);
 
 	return (
 		<div style={{fontSize: '2em'}}>
-			<Filters activityType={activityTypeFilter} setActivityType={setActivityTypeFilter} style={{marginBottom: '1em'}} />
+			<Filters activityType={activityTypeFilter} setActivityType={setActivityType} style={{marginBottom: '1em'}} />
 
 			<FilterContext.Provider value={activityTypeFilter}>
 				<ActivityList activities={allActivities} hLevel={2} />
 			</FilterContext.Provider>
 		</div>
 	);
+}
+
+function getActivityTypeFromUrl(): null | ActivityType {
+	const parameters = new URLSearchParams(window.location.search);
+	const activityTypeParameter = parameters.get('activityType');
+	if (!activityTypeParameter) {
+		return null;
+	}
+
+	if (!Object.values(ActivityType).includes(activityTypeParameter as ActivityType)) {
+		return null;
+	}
+
+	return activityTypeParameter as ActivityType;
 }
 
 export default NestedActivityList;
