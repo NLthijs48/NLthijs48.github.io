e0339e1572ed734740e014eaabcb901562f7b917
Thijs Wiefferink <thijs@wiefferink.me>
2023-01-30 22:50:31 +0100

    Fix timespan for KOSC
    Found the actual dates on my LinkedIn :P

--- src/activities/kosc/kosc.ts
+++ src/activities/kosc/kosc.ts
@@ -6,8 +6,8 @@ const kosc: HobbyInformation = {
 	activityType: ActivityType.Hobby,
 	name: 'KOSC',
 	description: 'Website and surrounding systems for a soccer club, as a volunteer as part of the IT committee.',
-	from: 'November 2011', // Joined as a volunteer
-	till: 'January 2017',
+	from: 'May 2011', // Joined as a volunteer
+	till: 'April 2018',
 	Icon: SportsSoccerIcon,
 	// Got replaced by a newer version
 	website: 'https://kosc.nl/',
