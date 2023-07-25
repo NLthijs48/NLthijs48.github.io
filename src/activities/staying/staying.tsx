e59068fe65a0437e573e08c72436b125e209a49a
Thijs Wiefferink <thijs@wiefferink.me>
2023-07-25 23:06:32 +0200

    staying: add guest app images, longer description

--- src/activities/staying/staying.tsx
+++ src/activities/staying/staying.tsx
@@ -2,23 +2,57 @@ import {JobInformation} from '../ActivityInformation';
 import RoomServiceIcon from '@mui/icons-material/RoomService';
 import React from 'react';
 import ActivityType from '../ActivityType';
+import HighlightType from '../HighlightType';
+
+import appHome from './appHome.png';
+import appHotel from './appHotel.png';
+import appChat from './appChat.png';
 
 const staying: JobInformation = {
 	activityType: ActivityType.Job,
 	name: 'Staying',
 	slug: 'staying',
-	oneliner: (
+	oneliner:
+		'Service to let hotel guests communicate with the front desk: before, during and after their stay to have all information they need about the accommodation and surroundings.',
+	description: (
 		<div>
-			Service to let hotel guests communicate with the front desk: before, during and after their stay to have all information they
-			need about the accommodation and surroundings.
+			The parts that made up the company:
+			<ul>
+				<li>Web portal for hotels to set up their hotel + chat with guests</li>
+				<li>App for guests to view their stay, information provided by the hotel and chat with the front desk</li>
+				<li>App for the hotel to chat with their guests</li>
+				<li>Website to onboard new customers</li>
+			</ul>
 			<div style={{marginTop: '0.5em'}}>
-				I worked parttime on the React admin portal and react-native app, while also doing my Master thesis here.
+				I worked parttime on the hotel portal and react-native apps, while working on my Master thesis in the remaining time at the
+				same company.
 			</div>
 		</div>
 	),
 	from: 'February 2017',
 	till: 'March 2018',
 	Icon: RoomServiceIcon,
+	highlights: [
+		{
+			type: HighlightType.Image,
+			title: 'Guest app home screen',
+			image: appHome,
+			caption: 'Home screen of the app for guests, showing the reservation they have at a hotel.',
+		},
+		{
+			type: HighlightType.Image,
+			title: 'Guest app hotel overview',
+			image: appHotel,
+			caption:
+				'Hotel page in the app for guests, showing all items the hotel configured. Can be HTML content, pdfs, map views, external links and more.',
+		},
+		{
+			type: HighlightType.Image,
+			title: 'Guest app chat',
+			image: appChat,
+			caption: 'Chat in the app for guests to contact the hotel about their reservation or any other questions about their stay.',
+		},
+	],
 };
 
 export default staying;
--- src/components/HighlightItem.tsx
+++ src/components/HighlightItem.tsx
@@ -12,7 +12,7 @@ function HighlightItem(props: HighlightItemProps) {
 		return (
 			<figure style={{margin: 0, marginBottom: '2em'}}>
 				<h3 style={{marginBottom: '0.1em'}}>{props.highlight.title}</h3>
-				<img src={props.highlight.image} alt={props.highlight.title} />
+				<img src={props.highlight.image} alt={props.highlight.title} style={{maxHeight: '50em'}} />
 				<figcaption style={{maxWidth: '40em'}}>{props.highlight.caption}</figcaption>
 			</figure>
 		);
