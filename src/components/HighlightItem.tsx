7ac72f1cd926a6fec3a5bd075d5ef110c975c379
Thijs Wiefferink <thijs@wiefferink.me>
2023-08-05 17:03:53 +0200

    HighlightItem: image rendering improvements
    - add icon in front of the title
    - move the caption above the image
    - conditionally render the caption (can be missing)

--- src/components/HighlightItem.tsx
+++ src/components/HighlightItem.tsx
@@ -17,13 +17,19 @@ function HighlightItem(props: HighlightItemProps) {
 		// TODO: use header similar to ActivityTitle?
 		return (
 			<figure style={{margin: 0, marginBottom: '2em', maxWidth: '100%'}}>
-				<h3 style={{marginBottom: '0.1em'}}>{props.highlight.title}</h3>
+				<figcaption>
+					<h3 style={{marginBottom: '0.1em'}}>
+						<Icon style={{fontSize: '1.2em', marginBottom: '-0.2em', marginLeft: '-0.1em', opacity: 0.7}} />{' '}
+						{props.highlight.title}
+					</h3>
+					{!!props.highlight.caption && <div style={{maxWidth: '40em', marginBottom: '0.5em'}}>{props.highlight.caption}</div>}
+				</figcaption>
+
 				<img
 					src={props.highlight.image}
 					alt={props.highlight.title}
 					style={{maxHeight: props.highlight.fullHeight ? undefined : '50em', maxWidth: '100%'}}
 				/>
-				<figcaption style={{maxWidth: '40em'}}>{props.highlight.caption}</figcaption>
 			</figure>
 		);
 	}
@@ -40,7 +46,7 @@ function HighlightItem(props: HighlightItemProps) {
 			<div style={{marginBottom: '2em'}}>
 				<ExternalLink href={props.highlight.href} underline="none">
 					<h3 style={{marginBottom: '0.1em'}}>
-						<Icon style={{fontSize: '1.1em', marginBottom: '-0.2em', marginLeft: '-0.1em'}} />{' '}
+						<Icon style={{fontSize: '1.2em', marginBottom: '-0.2em', marginLeft: '-0.1em'}} />{' '}
 						<span style={{textDecoration: 'underline'}}>{props.highlight.title}</span>
 					</h3>
 				</ExternalLink>
