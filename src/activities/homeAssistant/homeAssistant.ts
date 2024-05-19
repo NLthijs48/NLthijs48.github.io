afd54b9678f0e298a4871faad5e2e028da027a5e
Thijs Wiefferink <thijs@wiefferink.me>
2024-05-19 23:09:10 +0200

    home assistant: add screenshot, improve link

--- src/activities/homeAssistant/homeAssistant.ts
+++ src/activities/homeAssistant/homeAssistant.ts
@@ -1,15 +1,31 @@
 import {HobbyInformation} from '../ActivityInformation';
 import HomeIcon from '@mui/icons-material/Home';
 import ActivityType from '../ActivityType';
+import HighlightType from '../HighlightType';
+import home from './home.png';
 
 const homeAssistant: HobbyInformation = {
 	activityType: ActivityType.Hobby,
 	name: 'Home Assistant dashboard',
 	slug: 'homeAssistant',
-	oneliner: 'Automating my home: lighting, home cinema, electricity usage.',
+	oneliner: 'Automating my home: lighting, home cinema, electricity usage, and a ton more.',
 	from: '2020',
 	Icon: HomeIcon,
-	website: 'https://github.com/NLThijs48/home-assistant',
+	highlights: [
+		{
+			type: HighlightType.Link,
+			href: 'https://github.com/NLThijs48/home-assistant',
+			title: 'GitHub source code for my Home Assistant configuration',
+		},
+		{
+			type: HighlightType.Image,
+			image: home,
+			title: 'My Home Assistant home screen',
+			caption:
+				'Example of what my home screen looks like: it highlights all the things important at this time (example where everything is happening at once). Tracking washing machine cycles, home cinema automation, grocery delivery tracking, air quality, electricity usage, weather forecasting, plant care, robot vacuum.',
+			fullHeight: true,
+		},
+	],
 };
 
 export default homeAssistant;
