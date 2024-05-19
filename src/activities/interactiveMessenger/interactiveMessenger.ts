cb7434bbc107c49faeeb5b9268b910597b983b6e
Thijs Wiefferink <thijs@wiefferink.me>
2024-05-19 23:26:47 +0200

    interactiveMessenger: improve links section

--- src/activities/interactiveMessenger/interactiveMessenger.ts
+++ src/activities/interactiveMessenger/interactiveMessenger.ts
@@ -1,6 +1,7 @@
 import {HobbyInformation} from '../ActivityInformation';
 import ChatIcon from '@mui/icons-material/Chat';
 import ActivityType from '../ActivityType';
+import HighlightType from '../HighlightType';
 
 const interactiveMessenger: HobbyInformation = {
 	activityType: ActivityType.Hobby,
@@ -12,7 +13,16 @@ const interactiveMessenger: HobbyInformation = {
 	// End of active development
 	till: 'December 2017',
 	Icon: ChatIcon,
-	website: 'https://github.com/NLthijs48/InteractiveMessenger',
+	highlights: [
+		{type: HighlightType.Link, href: 'https://github.com/NLthijs48/InteractiveMessenger', title: 'Source code on GitHub'},
+		{
+			type: HighlightType.Link,
+			href: 'https://github.com/NLthijs48/InteractiveMessenger/blob/master/Specifications%20%26%20Guidelines.md',
+			title: 'Message specification',
+			caption: 'Examples of inputs and the messages they produce in MineCraft',
+		},
+		{type: HighlightType.Link, href: 'https://interactivemessenger.wiefferink.me/', title: 'JavaDoc of the library'},
+	],
 };
 
 export default interactiveMessenger;
