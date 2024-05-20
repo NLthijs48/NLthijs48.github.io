9629a15f46e6e621f85d016931d2fb073de253b5
Thijs Wiefferink <thijs@wiefferink.me>
2024-05-20 19:46:24 +0200

    playerWarps: improve description and links

--- src/activities/playerWarps/playerWarps.ts
+++ src/activities/playerWarps/playerWarps.ts
@@ -1,16 +1,18 @@
 import {HobbyInformation} from '../ActivityInformation';
 import AirlineStopsIcon from '@mui/icons-material/AirlineStops';
 import ActivityType from '../ActivityType';
+import HighlightType from '../HighlightType';
 
 const playerWarps: HobbyInformation = {
 	activityType: ActivityType.Hobby,
 	name: 'PlayerWarps',
 	slug: 'minecraftPlayerWarps',
 	oneliner: 'Minecraft server plugin to let players teleport to a list of saved in-game locations',
+	description: 'Mostly only used for my own Minecraft server, but made open source for others to use as well.',
 	from: 'January 2016', // Estimate, first commit in July 2016, but the plugin has been created and used before that
 	till: 'October 2017', // End of active development
 	Icon: AirlineStopsIcon,
-	website: 'https://github.com/NLthijs48/PlayerWarps',
+	highlights: [{type: HighlightType.Link, href: 'https://github.com/NLthijs48/PlayerWarps', title: 'Source code on GitHub'}],
 };
 
 export default playerWarps;
