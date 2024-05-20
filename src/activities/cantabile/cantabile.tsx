b93a1bcf7d5f5005d156f0ed0254c5771d221dbc
Thijs Wiefferink <thijs@wiefferink.me>
2024-05-20 20:36:06 +0200

    cantabile: add logo image

--- src/activities/cantabile/cantabile.tsx
+++ src/activities/cantabile/cantabile.tsx
@@ -4,14 +4,23 @@ import ActivityType from '../ActivityType';
 import HighlightType from '../HighlightType';
 import members from './members.png';
 import contact from './contact.png';
+import logo from './logo.png';
+import React from 'react';
 
 const cantabile: JobInformation = {
 	activityType: ActivityType.Job,
 	slug: 'cantabile',
 	name: 'Cantabilé',
 	oneliner: 'Website for a female choir, worked together with designer Stef Heerink.',
-	description:
-		'Simple website using the Joomla CMS with a custom template styled for the choir. It contained some static pages, but also a calendar with events, book for guests to write in and a photos section.',
+	description: (
+		<div>
+			<img src={logo} alt="Cantabile logo: simple wordmark with handwritten font" style={{height: 80}} />
+			<p>
+				Simple website using the Joomla CMS with a custom template styled for the choir. It contained some static pages, but also a
+				calendar with events, book for guests to write in and a photos section.
+			</p>
+		</div>
+	),
 	from: 'May 2012',
 	till: 'June 2012',
 	Icon: MusicNoteIcon,
