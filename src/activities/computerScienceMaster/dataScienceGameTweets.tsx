1c578effc17fa254eff4c65620bdaa5f4919d6af
Thijs Wiefferink <thijs@wiefferink.me>
2024-05-20 21:06:43 +0200

    gameTweets: add image

--- src/activities/computerScienceMaster/dataScienceGameTweets.tsx
+++ src/activities/computerScienceMaster/dataScienceGameTweets.tsx
@@ -3,6 +3,7 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import React from 'react';
 import ActivityType from '../ActivityType';
 import HighlightType from '../HighlightType';
+import assassinsCreedGraph from './dataScienceGameTweets/assassinsCreedGraph.jpg';
 
 const dataScienceGameTweets: EducationInformation = {
 	activityType: ActivityType.Education,
@@ -25,6 +26,13 @@ const dataScienceGameTweets: EducationInformation = {
 			title: 'Paper (pdf)',
 		},
 		// TODO: link to the results page (Github pages on the GameTweets repo?)
+		{
+			type: HighlightType.Image,
+			image: assassinsCreedGraph,
+			title: "Assassin's Creed launch graph",
+			caption:
+				'The blue line shows the number of tweets about the game, as analyzed by us using a huge Twitter dataset. The green, black and orange lines show sales numbers of the main platforms the games was released on.',
+		},
 	],
 };
 
