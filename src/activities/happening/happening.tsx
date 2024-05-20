a83c216437ae6853a393419e1dc61d6b7238ff07
Thijs Wiefferink <thijs@wiefferink.me>
2024-05-20 19:58:12 +0200

    happening: add logo

--- src/activities/happening/happening.tsx
+++ src/activities/happening/happening.tsx
@@ -7,15 +7,22 @@ import splitTheBill from './splitTheBill';
 import whereabouts from './whereabouts';
 import commitLog from './commitLog';
 import HighlightType from '../HighlightType';
+import logo from './logo.png';
 
 const happening: JobInformation = {
 	activityType: ActivityType.Job,
 	name: 'Happening',
 	slug: 'happening',
-	oneliner: (
+	oneliner: 'Supercharged group chat app with tools, games and more',
+	description: (
 		<div>
-			I was a developer at Happening, helping to improve the core of this group chat app, and writing games and tools that can be used
-			in those groups.
+			<div>
+				<img src={logo} alt="Happening logo: robotic smiley face in white on a blue background" style={{height: 80}} />
+			</div>
+			<p>
+				I was a developer at Happening, helping to improve the core of this group chat app, and writing games and tools that can be
+				used in those groups. See the projects below for a couple of the plugins I wrote.
+			</p>
 		</div>
 	),
 	from: 'July 2015',
